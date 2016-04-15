d38e82ea7a8c09414e7fc7af5418606ec154515e
Troy Whorten <sabotuer99@gmail.com>
2016-04-15 00:01:06 +0000

    forcing https

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module Hackathon
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
+    config.force_ssl = true
   end
 end
