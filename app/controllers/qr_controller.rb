fbbdcb3e4a7fa47563aa5ee81474e0b3a41dce14
Troy Whorten <sabotuer99@gmail.com>
2016-04-15 05:36:25 +0000

    weird bug with qr code gen

--- app/controllers/qr_controller.rb
+++ app/controllers/qr_controller.rb
@@ -6,7 +6,7 @@ class QrController < ApplicationController
   
   def index
     base = "https://chart.googleapis.com/chart?cht=qr&chs=150x150&choe=UTF-8&chld=H&chl="
-    @codes = Item.all.collect do |item|
+    @codes = Item.all.take(20).collect do |item|
        qr = {}
        qr['item'] = item
        #qr['code'] = RQRCode::QRCode.new( request.protocol + request.host + edit_item_path(item), :size => 6, :level => :h )
@@ -16,3 +16,4 @@ class QrController < ApplicationController
   end
   
 end
+
